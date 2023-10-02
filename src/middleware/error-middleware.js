bd7fc2c05ed6e3fee77424343a5ee98fa14eba7a
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-10-02 22:05:08 +0700

    configuration error middleware

--- src/middleware/error-middleware.js
+++ src/middleware/error-middleware.js
@@ -1,4 +1,4 @@
-import { ResponseError } from "../error/response-error";
+import { ResponseError } from "../error/response-error.js";
 
 export const errorMiddleware = (err, req, res, next) => {
     if (!err) {
