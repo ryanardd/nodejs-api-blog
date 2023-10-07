d00683295e1b5144ab18ec4289573f5284e56a45
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-10-07 23:35:56 +0700

    server config

--- src/app/web.js
+++ src/app/web.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { route } from "../router/api.js";
+import { publicRoute } from "../router/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
+import { route } from "../router/api.js";
 
 export const app = express();
 app.use(express.json());
 
+app.use(publicRoute);
 app.use(route);
 
 app.use(errorMiddleware);
