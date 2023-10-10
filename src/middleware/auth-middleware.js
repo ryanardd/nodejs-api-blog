a695b1fc0e2454d3d77e72a0e092b8f20806e26f
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-10-10 21:51:07 +0700

    init auth

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -2,40 +2,21 @@ import jwt from "jsonwebtoken";
 
 export const authMiddleware = async (req, res, next) => {
     const bearer = req.headers["authorization"];
-    const token = bearer.split(" ")[1];
+    const token = bearer && bearer.split(" ")[1];
 
-    console.log(token);
+    try {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.status(403).json({
+                    errors: "Forbidden",
+                });
+            }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({
-                errors: "Forbidden",
-            });
-        }
-
-        req.user = user;
-
-        next();
-    });
-    // if (!token) {
-    //     return res.status(401).json({
-    //         errors: "Unauthorization",
-    //     });
-    // }
-
-    // try {
-    //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    //         if (err) {
-    //             return res.status(403).json({
-    //                 errors: "Forbidden",
-    //             });
-    //         }
-
-    //         req.user = user;
-
-    //         next();
-    //     });
-    // } catch (error) {
-    //     next(error);
-    // }
+            req.user = user;
+            // return user;
+            next();
+        });
+    } catch (error) {
+        next(error);
+    }
 };
