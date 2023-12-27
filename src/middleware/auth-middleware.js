f0a72bf30e3a3fb2b8895f980a1b1c914cc50f45
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-12-27 13:37:54 +0700

    auth validation

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,22 +1,19 @@
 import jwt from "jsonwebtoken";
+import { ResponseError } from "../error/response-error";
 
 export const authMiddleware = (req, res, next) => {
-    // const token = req.cookies.token;
-    const token = req.headers.authorization;
-    // console.info(user);
+    const token = req.cookies.token;
+
     if (!token) {
         return res.status(401).json({ message: "Token no provided" });
-    }
-
-    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    if (decode === req.cookies.token) {
-        req.user = decode;
-    }
-
-    if (token !== decode) {
-        res.status(403).json({
-            errors: "Forbidden",
+    } else {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+            if (err) {
+                throw new ResponseError(401, "invalid token");
+            } else {
+                req.user = decode.user;
+                next();
+            }
         });
     }
 };
