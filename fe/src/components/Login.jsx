d07e76dabb9e76fc89b54a03beb12278d2f2e01e
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2024-02-14 22:04:29 +0700

    setup

---
+++ fe/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button"
+
+
+export const Login = () => {
+    return (
+        <div className="bg-slate-600 h-screen">
+            <div className="container border">
+                <div className="h-full">
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Login } from './components/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login></Login>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
